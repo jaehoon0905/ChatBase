b1e3bbaff42d965404e8d7ebd623066e1680eeca
Lee Jaehoon <jaehoonlee5139@gmail.com>
2025-03-30 19:39:15 +0900

    Remove development tools opening from main.js

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ function createWindow() {
 
   // 메뉴 제거
   Menu.setApplicationMenu(null);
-  win.webContents.openDevTools();
 
   // React 앱이 빌드된 public 폴더의 index.html을 로드
   win.loadFile(path.join(__dirname, 'public', 'index.html'));
